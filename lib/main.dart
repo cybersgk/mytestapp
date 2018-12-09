43d23a1486ba8c77972928e9b8b95250ba5dff2d
cybersgk <cybersgk@gmail.com>
2018-12-09 21:20:44 +0530

    homecatalog

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Home Catalog',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,14 +18,14 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFFd3a028),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Home Catalog'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -39,25 +39,7 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
+    @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
@@ -65,47 +47,63 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    List<String> familyMembers = ['Sivanand KC','Bindu U','Selva Kumar S','Ganesh Kumar S'];
     return Scaffold(
+      backgroundColor: Color(0xFF098469),
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          this.title,
+          style: TextStyle(color:Color(0xFF098469)),
+          ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 0.8,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+        children: List.generate(familyMembers.length, (index){
+          return Container(
+        padding:EdgeInsets.only(top: 20),
+            child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    'images/unknown_contact.jpg',
+                    height: 100.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:10.0),
+                    child: Text(familyMembers[index],
+                    style: TextStyle(color: Color(0xFFe5d84b), fontWeight: FontWeight.bold,fontSize: 16),
+                    textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              decoration: myBoxDecoration(),
+          );
+        }),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        // onPressed: _incrementCounter,
+        backgroundColor: Color(0xFFd3a028),
+        tooltip: 'Add Family Member',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+}
+
+BoxDecoration myBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(
+      width: 2.0,
+      color: Color(0xFFe5d84b)
+    ),
+    borderRadius: BorderRadius.all(
+        Radius.circular(15.0) //                 <--- border radius here
+    ),
+  );
 }
